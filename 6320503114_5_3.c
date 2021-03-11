8a51f05440da04dd0a39868767fb8403d9c96077
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-11 16:18:39 +0700

    1

---
+++ 6320503114_5_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,j,n,find,mod=2,check=0;
+    scanf("%d",&n);
+    for(i=1;j<n;i++)
+    {
+
+    }
+    printf("%d\n",n+check);
+}
+
+
+
