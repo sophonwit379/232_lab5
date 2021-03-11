3d6e88050f44af3790ac719fa461feb9c2ab4ade
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-11 17:41:04 +0700

    5

--- 6320503114_5_2.c
+++ 6320503114_5_2.c
@@ -1,23 +1,36 @@
 #include<stdio.h>
 int main ()
 {
-    int n,i,j,k,r,m;
+    int n,i,j,k,r,m,c;
     scanf("%d",&n);
     if(n%2==0)
     {
         m=n/2;
         r=n-1;
+        c=m;
     }
     else
     {
         m=(n+1)/2;
         r=n;
+        c=m-1;
     }
     for(i=0;i<m;i++)
     {
         for(j=0;j<r;j++)
         {
-            if(j==m+i-1 || j==m-i-1)
+            if(j==m-i-1 || j==m+i-1)
+                printf("*");
+            else
+                printf("_");
+        }
+        printf("\n");
+    }
+    for(i=c;i>0;i--)
+    {
+        for(j=r;j>0;j--)
+        {
+            if(j==m+i-1 || j==m-i+1)
                 printf("*");
             else
                 printf("_");
